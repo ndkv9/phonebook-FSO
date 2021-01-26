e59712e2f1dd9c1e8cb8fe649ff7b55a97489402
Vu Nguyen <vu.nguyen101@gmail.com>
2021-01-26 21:24:30 +0200

    add Person component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ const App = () => {
 	const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
 	const [newName, setNewName] = useState('')
 
+	const handleChange = e => {
+		setNewName(e.target.value)
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		setPersons(persons.concat(newName))
+		setNewName('')
+	}
+
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<div>
-					name: <input />
+					name: <input value={newName} onChange={handleChange} />
 				</div>
 				<div>
 					<button type='submit'>add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			...
 		</div>
 	)
 }
