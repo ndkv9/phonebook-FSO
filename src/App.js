126dfc83818b91536c1ff276a62a03485bb6587b
Vu Nguyen <vu.nguyen101@gmail.com>
2021-01-27 20:16:32 +0200

    fetch db with axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Person from './components/Person'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456' },
-		{ name: 'Ada Lovelace', number: '39-44-5323523' },
-		{ name: 'Dan Abramov', number: '12-43-234345' },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122' },
-	])
+	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
 	const [filter, setFilter] = useState('')
 
+	useEffect(() => {
+		axios.get('http://localhost:3001/persons').then(res => setPersons(res.data))
+	})
 	const handleNameChange = e => {
 		setNewName(e.target.value)
 	}
